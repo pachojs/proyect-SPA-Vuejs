5ba8404ab99e32a8e1f108dae76a058317b0b87d
pacho <pacho2390@gmail.com>
2023-11-07 20:54:46 -0500

    proyecto con vue SPA confecciónes tecnotextiles

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const apiRoutes = require('./routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//instancia de express en mi app
+const app = express();
+app.use(cors());
+
+//middleware morgan para detectar peticiones
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//Manejador de rutas
+app.use('/api', apiRoutes);
+
+
+//Server
+app.set('PORT', 3000)
+
+app.listen(app.get('PORT'), ()=>{
+    console.log('server up');
+});
+
+
+
+
